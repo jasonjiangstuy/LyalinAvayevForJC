8991bcd349444a185556ef346066f8ed81eb712f
jasonjiangstuy <jasonjiangny@gmail.com>
2020-08-24 17:00:19 -0400

    our experience MVP done

---
+++ js/ourExperience.js
@@ -0,0 +1,30 @@
+
+// // start
+// $(function(){
+    
+//     $('#mainNav').hide();
+//     var mainNav = $('#mainNav');
+//     var thresh = Number($('#showNav').offset().top);
+//     $(window).resize(function(){
+//         thresh = Number($('#showNav').offset().top);
+//         console.log(thresh);
+//     })
+//     var showing = false
+//     $(window).scroll(function(){
+//         console.log($(document).scrollTop());
+//         if (Number($(document).scrollTop()) > thresh) {
+//             if (showing == false){
+//             mainNav.slideToggle();
+//             console.log('showing');
+//             showing = true;
+//             }
+//         } else {
+//             if (showing == true){
+//             mainNav.hide();
+//             console.log('hiding');
+//             showing = false;
+//         }
+//     }
+// });
+// });
+
